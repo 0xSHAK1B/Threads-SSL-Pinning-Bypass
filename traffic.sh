1e7e5cb7b72b214526c1196ba6cd07078ef260ef
MUHAMMAD SHAKIB <mdshakibsheikh8833@gmail.com>
2026-03-12 04:57:33 +0600

    Update traffic.sh

--- traffic.sh
+++ traffic.sh
@@ -1,95 +1,48 @@
 POST /api/v1/bloks/async_action/com.bloks.www.bloks.caa.login.async.send_login_request/ HTTP/2
-
 host: i.instagram.com
-
 accept-language: en-US
-
 content-type: application/x-www-form-urlencoded; charset=UTF-8
-
 ig-intended-user-id: 0
-
 priority: u=3
-
 x-bloks-is-layout-rtl: false
-
 x-bloks-prism-button-version: CONTROL
-
 x-bloks-prism-colors-enabled: false
-
 x-bloks-prism-extended-palette-gray: false
-
 x-bloks-prism-extended-palette-indigo: false
-
 x-bloks-prism-extended-palette-polish-enabled: false
-
 x-bloks-prism-extended-palette-red: false
-
 x-bloks-prism-extended-palette-rest-of-colors: false
-
 x-bloks-prism-font-enabled: true
-
 x-bloks-prism-indigo-link-version: 0
-
-x-bloks-version-id: b6402fcf84a884f9e8d5342cacb3fab4b29f20745bac480e43893d269930cd45
-
+x-bloks-version-id: 58b4888388a4950367bf72e65b7210a4c6f999e076ced959fef9cba180f07a05
 x-fb-client-ip: True
-
 x-fb-connection-type: WIFI
-
 x-fb-friendly-name: IgApi: bloks/async_action/com.bloks.www.bloks.caa.login.async.send_login_request/
-
 x-fb-request-analytics-tags: {"network_tags":{"product":"3419628305025917","surface":"undefined","request_category":"api","purpose":"fetch","retry_attempt":"0"}}
-
 x-fb-server-cluster: True
-
 x-ig-android-id: android-37e8a9af442da8ad
-
 x-ig-app-id: 3419628305025917
-
 x-ig-app-locale: en_US
-
-x-ig-attest-params: "I removed that cuz it was to long 🐸🐸"
-
-x-ig-bandwidth-speed-kbps: 428.000
-
+x-ig-attest-params: 
+x-ig-bandwidth-speed-kbps: 337.000
 x-ig-bandwidth-totalbytes-b: 57399
-
-x-ig-bandwidth-totaltime-ms: 134
-
+x-ig-bandwidth-totaltime-ms: 170
 x-ig-client-endpoint: com.bloks.www.caa.login.home_template:com.bloks.www.caa.login.home_template
-
 x-ig-capabilities: 3brTv10=
-
 x-ig-connection-type: WIFI
-
-x-ig-device-id: 6d236494-3663-4552-9c91-e9c3d3ef2595
-
+x-ig-device-id: e8f9e29f-6663-4579-80f8-a2e30faaa793
 x-ig-device-locale: en_US
-
-x-ig-family-device-id: 657f472e-df54-498a-be5e-b9b48841b6c1
-
+x-ig-family-device-id: 71bf4983-1b6f-4640-aac4-2c7f947b9b27
 x-ig-is-foldable: false
-
 x-ig-mapped-locale: en_US
-
 x-ig-timezone-offset: 21600
-
 x-ig-www-claim: 0
-
-x-mid: aak-ewABAAG714V6lwzJ755kUx7O
-
-x-pigeon-rawclienttime: 1772710800.681
-
-x-pigeon-session-id: UFS-041ceb92-4e83-4497-9c80-e899f1805fff-0
-
+x-mid: abHw0QABAAEgjJmf5DH9HgzyxeIH
+x-pigeon-rawclienttime: 1773269215.512
+x-pigeon-session-id: UFS-4166c727-82fe-42d0-9859-1c087a241525-0
 x-tigon-is-retry: False
-
 accept-encoding: zstd
-
-content-length: 4143
-
-user-agent: Barcelona 419.0.0.48.71 Android (30/11; 274dpi; 720x1640; Symphony; Z35; Z35; mt6765; en_US; 896333562)
-
-x-fb-conn-uuid-client: 6ef4534b5b39487367c5a88c16caeaac
-
+content-length: 4157
+user-agent: Barcelona 420.0.0.58.72 Android (30/11; 274dpi; 720x1640; Symphony; Z35; Z35; mt6765; en_US; 904577743)
+x-fb-conn-uuid-client: 2df60538d8c85677c26dc56adfae6131
 x-fb-http-engine: Tigon/MNS/TCP
